9082b40181d3d48d1cf083b7b69e6c6a1694d787
Daese <daeseonglee7@gmail.com>
2024-07-04 20:52:08 +0900

    JSON 기초 Study 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                setData([...json]);
+                console.log(json);
+            });
+    }, []);
+    return (
+        <div>
+            <h3>JSONPlaceholder DATA</h3>
+            {data.map((Data) => {
+                return (
+                    <div
+                        style={{ border: '1px solid black', margin: '5px' }}
+                        key={Data.id}
+                    >
+                        <ul>
+                            <li>{Data.userId}</li>
+                            <li>{Data.id}</li>
+                            <li>{Data.title}</li>
+                            <li>{Data.body}</li>
+                        </ul>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
